db80876f5fcd370c6567f392347cc0baa7f1233b
Daniel Truong <ddtruong06@gmail.com>
2020-04-29 21:30:47 -0700

    Initial Commit

---
+++ WordSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordSearch
+//
+//  Created by Daniel Truong on 4/29/20.
+//  Copyright © 2020 Daniel Truong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
